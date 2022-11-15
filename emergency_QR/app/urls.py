642903d9df31869b7c6bf06055993dac3dc9a1f9
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2022-11-15 11:13:28 +0530

    jnhgfds

---
+++ emergency_QR/app/urls.py
@@ -0,0 +1,9 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
+]
